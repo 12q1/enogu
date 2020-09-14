be37c52271a92d4e9955673c86ce3a8cd7ff7d1c
12q1 <thomasham89@gmail.com>
2020-09-15 00:00:20 +0200

    added vanilla konva library

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { Stage, Layer, Rect, Circle } from 'react-konva';
 
 class Canvas extends Component {
-  render() {
-    return <h1>Canvas goes here</h1>;
-  }
+    render() {
+        return (
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    <Rect width={50} height={50} fill="red" />
+                    <Circle x={200} y={200} stroke="black" radius={50} />
+                </Layer>
+            </Stage>
+        )
+    }
 }
 
 export default Canvas;
